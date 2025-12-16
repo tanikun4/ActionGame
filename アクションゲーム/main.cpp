cd71beba9f6c3d8669ffad4e7332601b282b2754
tanikun4 <tanikun410@gmail.com>
2025-12-16 19:12:59 +0900

    mainからWinMainに変更、デバッグ時はコンソールを出す

--- アクションゲーム/main.cpp
+++ アクションゲーム/main.cpp
@@ -4,17 +4,54 @@
 //=======================================
 //エントリーポイント
 //=======================================
-int main(void)
+//int main(void)
+//{
+//#if defined(DEBUG) || defined(_DEBUG)
+//    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+//#endif//defined(DEBUG) || defined(_DEBUG)
+//
+//    //std::cout << "あああ" << endl;
+//
+//    // アプリケーション実行
+//    Application app(SCREEN_WIDTH,SCREEN_HEIGHT);
+//    app.Run();
+//
+//    return 0;
+//}
+
+
+#include <windows.h>
+#include <crtdbg.h>
+
+//=======================================
+// エントリーポイント（WinMain）
+//=======================================
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE,
+    LPSTR,
+    int nCmdShow
+)
 {
 #if defined(DEBUG) || defined(_DEBUG)
+    // メモリリーク検出
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif//defined(DEBUG) || defined(_DEBUG)
 
-    //std::cout << "あああ" << endl;
+    // コンソール生成
+    AllocConsole();
+
+    // printf / cout をコンソールに接続
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+#endif
 
     // アプリケーション実行
-    Application app(SCREEN_WIDTH,SCREEN_HEIGHT);
+    Application app(SCREEN_WIDTH, SCREEN_HEIGHT);
     app.Run();
+
+#if defined(DEBUG) || defined(_DEBUG)
+    FreeConsole();
+#endif
 
     return 0;
 }
